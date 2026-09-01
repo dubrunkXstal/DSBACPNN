91f2bfc3161db2703783c96254dee0d034b9a0b9
Dobrynia <dobryrobry@gmail.com>
2026-09-02 01:32:35 +0300

    style: clang-format blackbox/

--- include/bbx/blackbox/BlackBox.hpp
+++ include/bbx/blackbox/BlackBox.hpp
@@ -1,28 +1,26 @@
 #pragma once
 
-#include <cstdio>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <memory>
-#include <vector>
-#include <filesystem>
-#include <algorithm>
-
 #include <Eigen/Core>
 #include <Eigen/Dense>
-
+#include <algorithm>
 #include <bbx/activationfunctions/ActivationFunctions.hpp>
 #include <bbx/activationfunctions/AnyActivationFunction.hpp>
 #include <bbx/blackbox/Block.hpp>
-#include <bbx/lossfunctions/LossFunctions.hpp>
 #include <bbx/lossfunctions/AnyLossFunction.hpp>
+#include <bbx/lossfunctions/LossFunctions.hpp>
 #include <bbx/types.hpp>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace bbx {
 
 class BlackBox {
-public:
+   public:
     BlackBox(std::ifstream& settings);
 
     BlackBox(std::initializer_list<BlockConfig> block_configs);
@@ -33,35 +31,32 @@ public:
 
     void tuning(const Matrix& x_batch, const Matrix& y_batch);
 
-    void loadTrainCSV(const std::filesystem::path& path, std::vector<int> target_cols_idx, char delimiter = ',', bool have_header = false, int batch_size = 32);
+    void loadTrainCSV(const std::filesystem::path& path, std::vector<int> target_cols_idx,
+                      char delimiter = ',', bool have_header = false, int batch_size = 32);
 
-    double loadTestCSV(const std::filesystem::path& path, std::vector<int> target_cols_idx, std::function<bool(const Vector&, const Vector&)> is_correct, char delimiter = ',', bool have_header = false);
+    double loadTestCSV(const std::filesystem::path& path, std::vector<int> target_cols_idx,
+                       std::function<bool(const Vector&, const Vector&)> is_correct, char delimiter = ',',
+                       bool have_header = false);
 
-    size_t getBlocksCount() const
-    {
-        return blocks_.size();
-    }
+    size_t getBlocksCount() const { return blocks_.size(); }
 
-    void setLoss(AnyLoss loss_function)
-    {
-        loss_ = loss_function;
-    }
+    void setLoss(AnyLoss loss_function) { loss_ = loss_function; }
 
-    void setOptimizer(const AnyOptimizer& optimizer, std::vector<int> blocks_idx)
-    {
+    void setOptimizer(const AnyOptimizer& optimizer, std::vector<int> blocks_idx) {
         for (int& id : blocks_idx) {
             blocks_[id]->setOptimizer(optimizer);
         }
     }
 
-private:
+   private:
     std::vector<std::unique_ptr<Block> > blocks_;
     AnyLoss loss_;
 };
 
 // Implementation
 
-inline BlackBox::BlackBox(std::ifstream& settings) : blocks_(std::vector<std::unique_ptr<Block> >()), loss_(L2NormSquared{}) {
+inline BlackBox::BlackBox(std::ifstream& settings)
+    : blocks_(std::vector<std::unique_ptr<Block> >()), loss_(L2NormSquared{}) {
     Index in_dim;
     Index out_dim;
     std::string activation;
@@ -82,21 +77,16 @@ inline BlackBox::BlackBox(std::ifstream& settings) : blocks_(std::vector<std::un
         } else if (activation == "relu") {
             blocks_.emplace_back(std::make_unique<Block>(in_dim, out_dim, Relu{}));
         } else {
-            throw std::runtime_error("BlackBox::BlackBox(): Specified activation function for the block is unknown.");
+            throw std::runtime_error(
+                "BlackBox::BlackBox(): Specified activation function for the block is unknown.");
         }
     }
 }
 
 inline BlackBox::BlackBox(std::initializer_list<BlockConfig> block_configs) : loss_(L2NormSquared{}) {
     for (const auto& config : block_configs) {
-        blocks_.emplace_back(
-            std::make_unique<Block>(
-                config.input_dimension,
-                config.output_dimension,
-                config.activation_function,
-                config.optimizer
-            )
-        );
+        blocks_.emplace_back(std::make_unique<Block>(config.input_dimension, config.output_dimension,
+                                                     config.activation_function, config.optimizer));
     }
 }
 
@@ -115,8 +105,7 @@ inline void BlackBox::tuning(const Vector& x, const Vector& y) {
     remember_output.emplace_back(std::make_unique<Vector>(blocks_[0]->evaluate(x)));
 
     for (int i = 1; i < getBlocksCount(); ++i) {
-        remember_output.emplace_back(
-            std::make_unique<Vector>(blocks_[i]->evaluate(*remember_output[i - 1])));
+        remember_output.emplace_back(std::make_unique<Vector>(blocks_[i]->evaluate(*remember_output[i - 1])));
     }
 
     RowVector u = loss_.gradient(*(remember_output[getBlocksCount() - 1]), y);
@@ -136,8 +125,7 @@ inline void BlackBox::tuning(const Matrix& x_batch, const Matrix& y_batch) {
 
     remember_output.emplace_back(std::make_unique<Matrix>(blocks_[0]->evaluate(x_batch)));
     for (int i = 1; i < getBlocksCount(); ++i) {
-        remember_output.emplace_back(
-            std::make_unique<Matrix>(blocks_[i]->evaluate(*remember_output[i - 1])));
+        remember_output.emplace_back(std::make_unique<Matrix>(blocks_[i]->evaluate(*remember_output[i - 1])));
     }
 
     Matrix u = Matrix::Zero(y_batch.cols(), y_batch.rows());
--- include/bbx/blackbox/Block.hpp
+++ include/bbx/blackbox/Block.hpp
@@ -13,74 +13,67 @@ struct BlockConfig {
     AnyActivation activation_function;
     AnyOptimizer optimizer;
 
-    BlockConfig(Index input_dimension, Index output_dimension, const AnyActivation& activation_function, const AnyOptimizer& optimizer = VanillaDescent{})
-        : input_dimension(input_dimension), output_dimension(output_dimension), activation_function(activation_function), optimizer(optimizer) {}
+    BlockConfig(Index input_dimension, Index output_dimension, const AnyActivation& activation_function,
+                const AnyOptimizer& optimizer = VanillaDescent{})
+        : input_dimension(input_dimension),
+          output_dimension(output_dimension),
+          activation_function(activation_function),
+          optimizer(optimizer) {}
 };
 
 class Block {
-public:
-    Block(Index input_dimension, Index output_dimension, const AnyActivation& sigma, const AnyOptimizer& optimizer = VanillaDescent{})
-      : input_dimension_(input_dimension),
-        output_dimension_(output_dimension),
-        A_(Matrix::Random(output_dimension, input_dimension)),
-        b_(Vector::Random(output_dimension)),
-        sigma_(sigma),
-        A_optimizer_(optimizer),
-        b_optimizer_(optimizer) {}
+   public:
+    Block(Index input_dimension, Index output_dimension, const AnyActivation& sigma,
+          const AnyOptimizer& optimizer = VanillaDescent{})
+        : input_dimension_(input_dimension),
+          output_dimension_(output_dimension),
+          A_(Matrix::Random(output_dimension, input_dimension)),
+          b_(Vector::Random(output_dimension)),
+          sigma_(sigma),
+          A_optimizer_(optimizer),
+          b_optimizer_(optimizer) {}
 
-    Vector evaluate(const Vector& x) const
-    {
-        return sigma_.evaluate(A_ * x + b_);
+    Vector evaluate(const Vector& x) const { return sigma_.evaluate(A_ * x + b_); }
+
+    Matrix evaluate(const Matrix& x_batch) const {
+        return ((A_ * x_batch).colwise() + b_).unaryExpr([this](double v) { return sigma_.evaluate(v); });
     }
 
-    Matrix evaluate(const Matrix& x_batch) const
-    {
-        return ((A_ * x_batch).colwise() + b_).unaryExpr([this](double v) {
-            return sigma_.evaluate(v);
-        });
-    }
-
-    Matrix grad_A(const Vector& x, const RowVector& u) const
-    {
+    Matrix grad_A(const Vector& x, const RowVector& u) const {
         Vector delta = sigma_.derivative(A_ * x + b_).array() * u.transpose().array();
         return delta * x.transpose();
     }
 
     Matrix grad_A(const Matrix& x_batch, const Matrix& u_batch) const;
 
-    Vector grad_b(const Vector& x, const RowVector& u) const
-    {
+    Vector grad_b(const Vector& x, const RowVector& u) const {
         return sigma_.derivative(A_ * x + b_).asDiagonal() * u.transpose();
     }
 
     Vector grad_b(const Matrix& x_batch, const Matrix& u_batch) const;
 
-    void gradientDescent(const Vector& x, const RowVector& u)
-    {
+    void gradientDescent(const Vector& x, const RowVector& u) {
         A_ += A_optimizer_.computeUpdate(grad_A(x, u));
         b_ += b_optimizer_.computeUpdate(grad_b(x, u));
     }
 
-    void gradientDescent(const Matrix& x_batch, const Matrix& u_batch)
-    {
+    void gradientDescent(const Matrix& x_batch, const Matrix& u_batch) {
         A_ += A_optimizer_.computeUpdate(grad_A(x_batch, u_batch));
         b_ += b_optimizer_.computeUpdate(grad_b(x_batch, u_batch));
     }
 
-    RowVector propogateBack(const Vector& x, const RowVector& u) const
-    {
+    RowVector propogateBack(const Vector& x, const RowVector& u) const {
         return u * sigma_.derivative(A_ * x + b_).asDiagonal() * A_;
     }
 
     Matrix propogateBack(const Matrix& x_batch, const Matrix& u_batch) const;
 
-    void setOptimizer(const AnyOptimizer& optimizer)
-    {
+    void setOptimizer(const AnyOptimizer& optimizer) {
         A_optimizer_ = optimizer;
         b_optimizer_ = optimizer;
     }
 
-private:
+   private:
     Index input_dimension_;
     Index output_dimension_;
 
@@ -94,39 +87,30 @@ private:
 
 // Implementation
 
-inline Matrix Block::grad_A(const Matrix& x_batch, const Matrix& u_batch) const
-{
+inline Matrix Block::grad_A(const Matrix& x_batch, const Matrix& u_batch) const {
     Matrix Z = (A_ * x_batch).colwise() + b_;
 
-    Matrix Delta = Z.unaryExpr([this](double v) {
-        return sigma_.derivative(v);
-    });
+    Matrix Delta = Z.unaryExpr([this](double v) { return sigma_.derivative(v); });
 
     Delta.array() *= u_batch.transpose().array();
 
     return Delta * x_batch.transpose();
 }
 
-inline Vector Block::grad_b(const Matrix& x_batch, const Matrix& u_batch) const
-{
+inline Vector Block::grad_b(const Matrix& x_batch, const Matrix& u_batch) const {
     Matrix Z = (A_ * x_batch).colwise() + b_;
 
-    Matrix Delta = Z.unaryExpr([this](double v) {
-        return sigma_.derivative(v);
-    });
+    Matrix Delta = Z.unaryExpr([this](double v) { return sigma_.derivative(v); });
 
     Delta.array() *= u_batch.transpose().array();
 
     return Delta.rowwise().sum();
 }
 
-inline Matrix Block::propogateBack(const Matrix& x_batch, const Matrix& u_batch) const
-{
+inline Matrix Block::propogateBack(const Matrix& x_batch, const Matrix& u_batch) const {
     Matrix Z = (A_ * x_batch).colwise() + b_;
 
-    Matrix Delta = Z.unaryExpr([this](double v) {
-        return sigma_.derivative(v);
-    });
+    Matrix Delta = Z.unaryExpr([this](double v) { return sigma_.derivative(v); });
 
     Delta.array() *= u_batch.transpose().array();
 

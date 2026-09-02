b8ec6d75fbe838ad526dc312cfd5b01127a944df
Dobrynia <dobryrobry@gmail.com>
2026-09-02 11:44:29 +0300

    feat: добавлены методы для смены функций активаций

--- include/bbx/blackbox/BlackBox.hpp
+++ include/bbx/blackbox/BlackBox.hpp
@@ -48,7 +48,7 @@ class BlackBox {
         loss_ = loss_function;
     }
 
-    void setActivation(const anyActivation& sigma, std::vector<int> blocks_idx)
+    void setActivation(const AnyActivation& sigma, std::vector<int> blocks_idx)
     {
         for (int& id : blocks_idx) {
             blocks_[id]->setActivation(sigma);
